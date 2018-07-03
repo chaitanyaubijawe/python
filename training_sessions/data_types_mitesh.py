66213730ac3526210b5f7efdc5d424402cb24e8b
Chaitanya Bijawe <chaitanya.bijawe@pb.com>
2018-07-03 11:03:20 +0530

    changes for dict and list and tuple :: mitesh

--- training_sessions/data_types_mitesh.py
+++ training_sessions/data_types_mitesh.py
@@ -65,7 +65,7 @@ l.extend(c)
 l.append(1)
 print("List after append :: ", l)
 
-## remove from List
+## remove from List search for occurences and remove first .
 l.remove(1)
 ## try del ??
 
@@ -107,9 +107,116 @@ comp =[ item*2 for item in range(1,200) if item % 2 == 0]
 ## [ element (operation) for element in iterable condition ]
 print("Comprehessions :  ", comp)
 
-l12 =[]
+l12 =[] # expressions...
 for i in range(1,200):
     print(i)
     l12.append(i)
 
 print(l12)
+
+
+
+new_copied_list = l12.copy()
+
+print("Shallow copy of list : ", new_copied_list)
+
+
+
+
+s1 = {1,2,3}
+s2 = {5,2,7}
+
+print (s1 & s2)
+
+print (s1 | s2)
+
+
+print (s1 ^ s2)
+print (s1 <= s2)
+print (s1 >= s2)
+
+a = "6"
+b = []
+
+s = {1,2}
+l = [12,23]
+t = ()
+t = (1,2,3)
+t = (s)
+t = (l)
+print("tuple : ", t)
+a = "7";
+
+b.append(1)
+print("tuple a= 7 ? : ", t)
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return str(self.__dict__)
+p = Person("chaitanya")
+#p.name = "chaitanya"
+t = (p)
+print("tuple Person: ", t)
+p.name = "Mitesh"
+print("tuple Person: ", t)
+
+
+
+d = {"z1":"12313", "z2" : "zzdsdsd"}
+
+print("Keys are :: ", d.keys())
+value = d["z1"]
+print("z1 is key and its value in dict is :: ", value)
+
+
+for key in d.keys():
+    print("d[key]", d[key], " :: d.get(key)", d.get(key))
+
+
+for value in d.values():
+    print("iterating on values: ", value)
+
+
+for key, value in d.items():
+    print("Key is ", key, " value is :: ", value)
+
+
+l = [["z1","12313"], ["key_2", "value_2"]]
+
+for ll in l:
+    print ("list inside list ", ll)
+
+
+l1= ["1", "2"]
+
+v1, v2 = l1;
+
+
+for key, value in l:
+    print("key ", key, "value is:::: ", value)
+
+
+#############################################
+
+l = [1,2,3]
+
+for item in l:
+    print("item : ", item)
+
+
+l = [[1,2,3]]
+
+for item in l:
+    print("item : ", item)
+
+########################
+
+d = {}
+print("empty dict :: ", d)
+
+d["US"] = "its is bc country"
+
+print("US  dict :: ", d)
