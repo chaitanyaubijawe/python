65e9521ffa88eb3b15f5ed1c81026eb947296dc3
Chaitanya Bijawe <chaitanya.bijawe@pb.com>
2018-12-08 14:53:06 +0530

    changes

--- batch_3/session_1/data_types.py
+++ batch_3/session_1/data_types.py
@@ -93,15 +93,105 @@ print(value)
 value = "X" in name
 print(value)
 
+## List...
+
+# - container.....
+# - it can hold dynamic range of elements...
+#definition....
+l = list()
+l = []
+
+l = ["one", "two", "three", 1, []]
+# accessing 0th postional element....
+print(l[0])
+l[0]= 1
+print("##########")
+for item in l:
+    print(item)
+
+
+l.append("another element")
+l.append("another item")
+l.append(0)
+l.append([])
+
+l.pop(0)
+l.clear()
+print("##########")
+
+for item in l:
+    print(item)
+
+# tuple::
+
+t = ()
+t = (1,2)
+t = tuple()
+t = (1,2,4, ["one","two"])
+
+
+print(t[0])
+
+#t[0] = "ONE" ## not allowed.... immutable....
+
+
+l = t[3]
+
+l.append(123132)
+
+print(t)
+
+
+for item in t:
+    print(item)
+
+
+# Dictionaries...
+
+# key: value
+# it cannot hold duplicate key..
+d = {}
+d = {"key":"value","key_2":"value_2"}
+d = {"key":"value","key_2":[123,1231], "key_3":(12,12,12)}
+
+print(d["key"])
+v = d["key"]
+
+d["key"] = "asdfasadas"
+print(v)
+for key in d:
+    print(key)
+
+
+for key in d:
+    print(key  + " ---- " + str(d[key]))
+
+
+print(type(d.keys()))
+
+for key in d.keys():
+    print(key  + " ---- " + str(d[key]))
+
+print(type(d.items()))
+
+for key,value in d.items():
+    print(key  + " ---- " + str(d[key]))
+
+
+for thing in d.items():
+    print(thing)
 
 
 
+# Set
 
+s = {}
+s = {"a", 2, 6,1}
+# unique elements..
+# Exercise operators for set... union intersection etc...
+# you cannot access set element bby using index.... s[0] will result error
 
-
-
-
-
-
-
-#
+print(s)
+for elem in s:
+    print(elem)
+# World Map : gather country/state/cities....
