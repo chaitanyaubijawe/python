63c7bfd99e8d9eafa65334405c9f1e89ed140608
Chaitanya Bijawe <chaitanya.bijawe@pb.com>
2018-12-12 20:47:50 +0530

    changes for conditional statement and modules

---
+++ batch_3/session_2/module_1.py
@@ -0,0 +1,2 @@
+def m1():
+    print("hi there.... from module_1")
---
+++ batch_3/session_2/module_2.py
@@ -0,0 +1,10 @@
+
+# from module_1 import m1
+# from module_1 import *
+import module_1 as mod_1
+
+def m2():
+    print("Inside module 2....")
+    mod_1.m1()
+
+m2()
