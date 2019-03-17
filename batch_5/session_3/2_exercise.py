28ac8cd66c7d45eaad841590147f57e041ef63c1
Chaitanya Bijawe <chaitanya.bijawe@pb.com>
2019-03-17 13:16:35 +0530

    changes for exercise

---
+++ batch_5/session_3/2_exercise.py
@@ -0,0 +1,16 @@
+'''
+s = "1 2 3 5 5 2 2 8 3 0 3 21 6 8 5"
+output ::
+
+1 - count is :: 1
+2 - count is :: 3
+3 - count is :: 3
+
+
+output ::
+
+1-5 - count is :: 10
+6-10 - count is :: 10
+
+
+'''
